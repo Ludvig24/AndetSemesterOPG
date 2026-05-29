6e08d50b5d6ae175d032f8e9300150f65bd88090
Laura Marie Balle Jensen <laje11@student.eadania.dk>
2026-05-29 09:12:11 +0200

    Lidt små ting flyttet og slettet lidt kode

--- AndetSemesterOPG/MainWindow.xaml.cs
+++ AndetSemesterOPG/MainWindow.xaml.cs
@@ -27,60 +27,12 @@ namespace AndetSemesterOPG
         {
                 InitializeComponent();
 
-            //Her er connectionstring til databasen, den skal bruges til at åbne en forbindelse til databasen
-            //string connectionString = "Server=localhost\\SQLEXPRESS; Database=AndetSemester;Trusted_Connection=True;TrustServerCertificate=True";
-            // her oprettes en SqlConnection objekt ved hjælp af connectionString, som vil blive brugt til at åbne en forbindelse til databasen
-            //SqlConnection dataBase = new SqlConnection(connectionString);
-
             festival = new FestivalWindow(this);
 
             DispatcherTimer timer = new DispatcherTimer();
             timer.Tick += new EventHandler(AutoCreateAttendee);
             timer.Interval = new TimeSpan(0, 0, 5); // Her sættes intervallet for timeren til 5 sekunder
             timer.Start();
-
-
-            
-
-
-            //TEST FACTORY
-            //TicketClient tClient = new TicketClient();
-            //EntranceEastFactory en = new EntranceEastFactory();
-            //ITicket a = tClient.OrderTicketCampA(en);
-            //camp.Content = a.DetermineCampName();
-            //entrance.Content = a.DetermineEntranceType();
-
-            //ReadAll
-            //DBConnection dbConnection = new DBConnection();
-            //List<Attendee> allAttendees = dbConnection.ReadAll();
-            //Test Create and ADD to database
-
-            //Her Laver vi kun en attendee og tilføjer den til databasen
-            //AttendeeService attendeeCreator = new AttendeeService(new AttendeeRepository(new DBConnection()), new AttendeeTestData(), new TicketClient());
-            //Attendee attendee = attendeeCreator.CreateAttendee();
-
-            //Attendee attendese = attendeeCreator.CreateAttendee();
-            //for (int i = 0; i < 10; i++)
-            //{
-            //    Attendee attendees = attendeeCreator.CreateAttendee();
-            //}
-
-            // Her åbnes forbindelsen og den vil automatisk lukke forbindelsen da der bruges using
-            /*using (dataBase)
-            {
-                dataBase.Open();
-
-                SqlCommand command = new SqlCommand("SELECT * FROM Attendee", dataBase);
-                SqlDataReader reader = command.ExecuteReader();
-
-                if (reader.Read())
-                {
-                    string Name = reader.GetString(reader.GetOrdinal("FirstName"));
-                    EksempelNavn.Content = Name;
-
-
-                }
-            }*/
         }
 
 
